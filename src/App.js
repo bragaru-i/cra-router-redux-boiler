364932d0a7f52a0402bcf74fb5ccebf40aa29513
fargusMD <bragaruion@gmail.com>
2020-07-21 15:46:04 +0400

    CRA project started.. SCSS modules and global variables

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      app runs
+      <h1>
+        HEading 1<div className="subheading-1"> subheading</div>
+      </h1>
+      <p> paragraph there 1</p>
+      <p className="text"> ordinary text goes here</p>
+      <p className="text text--sm text--light"> ordinary text goes here</p>
+      <p className="text text--lg text--bold"> ordinary text goes here</p>
     </div>
   );
 }
